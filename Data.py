234588354b0208c101a5e3c6de35bfbbd4f742cc
Gabriel Delgado <128834773+GaabDelgado@users.noreply.github.com>
2023-05-16 18:02:17 -0300

    Add files via upload
    Atividades feitas em casa apenas por curiosidade e vontade de evoluir.

---
+++ Data.py
@@ -0,0 +1,10 @@
+print('''
+################################
+######### Data A/M/D ###########
+################################
+''')
+
+from datetime import datetime
+
+data_atual = datetime.now()
+print(data_atual.date())
